e950dab259b0133bc0af6c6f9b5e528870dedaca
Kunkuma Geeth Prasanna <kunkumagp@gmail.com>
2021-07-22 21:05:37 -0700

    1

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-// User: TheCodeholic
 
-echo "Hello World";
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
+
+$app = new Application;
+
+$app->router->get('/',function(){
+    return 'Hello world 2!';
+});
+
+$app->router->get('/contact',function(){
+    return 'Contact Page';
+});
+
+$app->run();
