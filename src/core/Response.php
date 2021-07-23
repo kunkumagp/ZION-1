37ad4980a90d193d0ceede8ba37e5c387bea2c22
Kunkuma Geeth Prasanna <kunkumagp@gmail.com>
2021-07-22 22:43:35 -0700

    2

---
+++ src/core/Response.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core;
+
+/*
+* @author     Kunkuma Geeth Prasanna <kunkumagp@gmail.com>
+* @package    app\core
+*/
+
+class Response  
+{
+    public function setStatusCode(int $code): void
+    {
+        http_response_code($code);
+    }
+}
---
+++ src/core/Router.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\core;
+
+/*
+* @author     Kunkuma Geeth Prasanna <kunkumagp@gmail.com>
+* @package    app\core
+*/
+
+class Router  
+{
+    public Request $request;
+    public Response $response;
+    protected array $routes = [];
+
+    public function __construct(Request $request, Response $response)
+    {
+
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function get($path,$callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false) {
+            $this->response->setStatusCode(404);
+            return "Not found";
+        }
+
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}',$viewContent,$layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.view.php";
+        return ob_get_clean();
+    }
+}
